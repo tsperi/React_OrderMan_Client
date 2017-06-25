da4045ec16fe84332ac0c6d27eda71b3577c1462
Mitch Masia <mjmasia@gmail.com>
2017-06-25 16:31:09 -0400

    Added renderTextField

--- app/views/common/forms/renderTextField.jsx
+++ app/views/common/forms/renderTextField.jsx
@@ -25,3 +25,64 @@
  *
  * @exports The pure functional component
  */
+
+
+// Module imports
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+/**
+ * @function renderTextField
+ * @description Renders a Bootstrap form input
+ *
+ * @param input
+ * @param label
+ * @param placeholder
+ * @param disabled
+ * @param type
+ */
+const renderTextField = ({ input, label, placeholder, disabled, type }) => (
+  <div className="form-group">
+    {(() => {
+      if (label) {
+        return (
+          <label
+            className="control-label"
+          >
+            {label}
+          </label>
+        );
+      }
+      return null;
+    })()}
+    <input
+      autoComplete="off"
+      className="form-control"
+      disabled={disabled}
+      placeholder={placeholder}
+      type={type}
+      {...input}
+    />
+  </div>
+);
+
+
+renderTextField.propTypes = {
+  disabled: PropTypes.bool,
+  input: PropTypes.any.isRequired,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+};
+
+
+renderTextField.defaultProps = {
+  disabled: false,
+  label: '',
+  placeholder: '',
+  type: 'text',
+};
+
+
+export default renderTextField;
